754d0555c1d9e88673ab8213c5fd411b91011f30
MrHoran8120 <fake@fakex.com>
2022-03-25 11:06:53 +1100

    Add project files.

---
+++ BlazorCar3/Globals.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorCar3
+{
+    public static class Globals
+
+    {
+        public static List<Car> cars = new List<Car>();
+
+
+        public static void addTestCars ()
+        {
+            cars.Add(new Car(0, "Green", 10));
+            cars.Add(new Car(1, "Blue", 20));
+            cars.Add(new Car(2, "Red", 30));
+        }
+
+    }
+
+    
+}
